d03b9395f9568a683d82aefcb22714f8dfdea5f9
Andrei_Bedruz <drei_2001@yahoo.com>
2022-05-16 11:04:34 +0800

    update main and options function

--- Conversions.java
+++ Conversions.java
@@ -16,26 +16,31 @@ public class Conversions
             sc.nextLine();
             switch (choice1)
             {
+                //upperCase
                 case 1:
                     System.out.println("Input String:");
                     string = sc.nextLine();
                     System.out.println("String = " + string.toUpperCase() + "\n\n");
                     break;
+                //lowerCase
                 case 2:
                     System.out.println("Input String:");
                     string = sc.nextLine();
                     System.out.println("String = " + string.toLowerCase() + "\n\n");
                     break;
+                //hasNumber
                 case 3:
                     System.out.println("Input String:");
                     string = sc.nextLine();
 
                     break;    
+                //isNumber
                 case 4:
                     System.out.println("Input String:");
                     string = sc.nextLine();
 
                     break;
+                //Conversions.
                 case 5:
                     break;
                 default:
@@ -53,10 +58,12 @@ public class Conversions
         System.out.println("String manipulation Software");
         System.out.println("[1] Convert String to upper Case");
         System.out.println("[2] Convert String to lower Case");
-        System.out.println("[3] Check if String is a valid number");
-        System.out.println("[4] Remove numbers in string and change case");
-        System.out.println("[5] Conversion System (meters and feet/ centimeters to inches");
-        System.out.println("[6] Exit");
+        System.out.println("[3] Check if String has a valid number");
+        System.out.println("[4] Check if String is a valid number");
+        System.out.println("[5] Remove numbers in string and change case");
+        System.out.println("[6] Conversion System (meters and feet/ centimeters to inches");
+        System.out.println("[7] Exit");
+        System.out.println("[8] Exit");
     }
 
 }
