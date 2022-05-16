fa433efd285a92c50258a7f5c0b81325f89dfc03
Andrei_Bedruz <20211617@student.curtin.edu.au>
2022-05-16 10:21:00 +0800

    add StringNumber template

---
+++ StringNumber.java
@@ -0,0 +1,17 @@
+public class StringNumber 
+{
+    public void checkForNums(String s)
+    {
+
+    }
+
+    public void checkIfNum(String s)
+    {
+
+    }
+
+    public void removeNumsToCase(String s, boolean strCase)
+    {
+
+    }
+}
