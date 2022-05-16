52708ec1fc70e6a1ca240259041b1161c6c46175
Andrei_Bedruz <drei_2001@yahoo.com>
2022-05-16 11:14:40 +0800

    update checkIfNum function

--- StringNumber.java
+++ StringNumber.java
@@ -10,14 +10,21 @@ public class StringNumber
                 char currChar = s.charAt(i);
                 int num = Character.getNumericValue(currChar);
                 System.out.println("Number" + num + " found at " + i + " place");
-            }
-            catch (NumberFormatException e) {}
+            } catch (NumberFormatException e) {}
         }
     }
 
     public void checkIfNum(String s)
     {
-
+        try 
+        {
+            float nums = Float.parseFloat(s);
+            System.out.println("String " + nums + " is a number");
+            
+        } catch (NumberFormatException e) 
+        {
+            System.out.println("String " + e + " is not a number");
+        }
     }
 
     public void removeNumsToCase(String s, boolean strCase)
