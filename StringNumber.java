3b03ef1c258c1f643ac7d358616a0bae9b9a9edc
Andrei_Bedruz <drei_2001@yahoo.com>
2022-05-16 10:57:15 +0800

    update CheckNums function

--- StringNumber.java
+++ StringNumber.java
@@ -2,7 +2,17 @@ public class StringNumber
 {
     public void checkForNums(String s)
     {
-
+        for(int i = 0;i < s.length();i++)
+        {
+            //try to change each glyph in the string to a 
+            try
+            {
+                char currChar = s.charAt(i);
+                int num = Character.getNumericValue(currChar);
+                System.out.println("Number" + num + " found at " + i + " place");
+            }
+            catch (NumberFormatException e) {}
+        }
     }
 
     public void checkIfNum(String s)
