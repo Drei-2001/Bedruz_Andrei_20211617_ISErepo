bc08519666b1146d31a1dbb1939216b152ccfeca
Andrei_Bedruz <20211617@student.curtin.edu.au>
2022-05-16 10:13:32 +0800

    Fixed while loop temporarily
    added Options, added Exit in one of the options for continues looping

--- Conversions.java
+++ Conversions.java
@@ -9,31 +9,22 @@ public class Conversions
         boolean endLoop = false;
         String string;
 
-        System.out.println("String manipulation Software");
-        System.out.println("[1] convert String to upper Case");
-        System.out.println("[2] convert String to lower Case");
-        System.out.println("[3] check if String is a valid number");
-        System.out.println("[4] remove numbers in string and change case");
-        System.out.println("[5] Conversion System (meters and feet/ centimeters to inches");
-
-        int choice1 = sc.nextInt();
-        
-        
-        sc.close();
-
         while(endLoop == false)
         {
+            Options();
+            int choice1 = sc.nextInt();
+            sc.nextLine();
             switch (choice1)
             {
                 case 1:
                     System.out.println("Input String:");
                     string = sc.nextLine();
-                    System.out.println("String = " + string.toUpperCase());
+                    System.out.println("String = " + string.toUpperCase() + "\n\n");
                     break;
                 case 2:
                     System.out.println("Input String:");
                     string = sc.nextLine();
-                    System.out.println("String = " + string.toLowerCase());
+                    System.out.println("String = " + string.toLowerCase() + "\n\n");
                     break;
                 case 3:
                     System.out.println("Input String:");
@@ -50,9 +41,22 @@ public class Conversions
                 default:
                     System.out.println("Wrong input. Please try again");
                     break;
+                case 6:
+                    endLoop = true;
             }
         }
-        
+        sc.close();
+    }
+
+    public static void Options()
+    {
+        System.out.println("String manipulation Software");
+        System.out.println("[1] Convert String to upper Case");
+        System.out.println("[2] Convert String to lower Case");
+        System.out.println("[3] Check if String is a valid number");
+        System.out.println("[4] Remove numbers in string and change case");
+        System.out.println("[5] Conversion System (meters and feet/ centimeters to inches");
+        System.out.println("[6] Exit");
     }
 
 }
