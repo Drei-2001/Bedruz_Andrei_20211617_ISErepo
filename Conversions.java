1e1811488c09d55a240a3b3cbb36cc1e7d5a9247
Andrei_Bedruz <drei_2001@yahoo.com>
2022-05-17 16:38:54 +0800

    ensure functions are static

---
+++ Conversions.java
@@ -0,0 +1,12 @@
+public class Conversions
+{
+    public static double hrToMin(double hours)
+    {
+        return hours*60;
+    }
+
+    public static double minToHr(double min)
+    {
+        return min/60;
+    }   
+}
