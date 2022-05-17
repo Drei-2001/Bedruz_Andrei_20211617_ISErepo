a48e3b12fd0c73b627808f69cb4edbc3354481db
Andrei_Bedruz <drei_2001@yahoo.com>
2022-05-17 16:27:21 +0800

    include conversions hrToMin and minToHr

--- Conversions.java
+++ Conversions.java
@@ -1,4 +1,12 @@
 public class Conversions
 {
+    public double hrToMin(double hours)
+    {
+        return hours*60;
+    }
 
+    public double minToHr(double min)
+    {
+        return min/60;
+    }   
 }
