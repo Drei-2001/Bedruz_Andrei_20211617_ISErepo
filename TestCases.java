261e06d6ffb4d3979542d3e256ab3d9f2effeccd
Andrei_Bedruz <drei_2001@yahoo.com>
2022-05-20 11:40:58 +0800

    create TestCase template

---
+++ TestCases.java
@@ -0,0 +1,42 @@
+public class TestCases
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+
+    public static void testToUpperCase()
+    {
+
+    }
+
+    public static void testToLowerCase()
+    {
+
+    }
+
+    public static void testCheckForNums()
+    {
+
+    }
+
+    public static void testCheckIfNums()
+    {
+
+    }
+
+    public static void testCheckRemoveNums()
+    {
+
+    }
+
+    public static void testHrToMin()
+    {
+
+    }
+
+    public static void testMinToHr()
+    {
+        
+    }
+}
